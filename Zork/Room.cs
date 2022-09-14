cab9e683f5698e4c08ad7897df6bff4a75f0b59f
HappyCtiv <zahar.sheyko@knights.ucf.edu>
2022-09-14 13:09:40 -0400

    Assignment 3.1
    Added Rooms file and changed the "LOOK" command to be more accurate

---
+++ Zork/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zork
+{
+    internal class Room
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Room(string name) //Constractor, intialize the member of the class
+        {
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+    }
+}
